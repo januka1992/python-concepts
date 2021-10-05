922be7233bc82e2f34f68bce4fb119da066e8e62
Varun Singh <varun@atliq.com>
2021-10-05 18:06:03 +0530

    Added 2nd assignment

---
+++ Assignments/Assignment_1_Python_Basics/Assignment_2.py
@@ -0,0 +1,14 @@
+# Assignment 2 - Check if two lists have at-least one element common
+
+def compare_lists(l1, l2):
+    # your code here. remove the line "pass" from this function once you add your code
+    pass
+    
+    
+if __name__ == '__main__':
+    str = "Asmita Is Learning Python"
+    list1 = [1, 2, 3, 4, 5]
+    list2 = [5, 6, 7, 8, 9]
+    print(compare_lists(list1, list2))
+    
+    
