3c1c052a9071c0dd5d8d13db2ef2abd9e07c8398
Varun Singh <varun@atliq.com>
2021-10-05 18:02:39 +0530

    Added first assignment

---
+++ Assignments/Assignment_1_Python_Basics/Assignment_1.py
@@ -0,0 +1,13 @@
+# Assignment 1 - Remove i’th character from given string
+
+def remove_ith_char(i):
+    # your code here. remove the line "pass" from this function once you add your code
+    pass
+    
+    
+if __name__ == '__main__':
+    str = "Asmita Is Learning Python"
+    i = 3
+    new_str = remove_ith_char(i)
+    print(new_str)
+    
